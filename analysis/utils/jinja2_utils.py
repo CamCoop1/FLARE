a986d995df203d11ef5ddec5718437b52038ed64
Cameron Cooper Harris <cameron.harris@adelaide.edu.au>
2025-01-09 16:30:27 +1030

    Refactored steering_script functions into the stages.py file as this felt more appropriate

--- analysis/utils/jinja2_utils.py
+++
@@ -1,9 +0,0 @@
-import jinja2
-
-from analysis.utils import find_file
-
-
-def get_template(name: str, dir="analysis/templates"):
-    loader = jinja2.FileSystemLoader(find_file(dir))
-    env = jinja2.Environment(loader=loader)
-    return env.get_template(name)
--- analysis/utils/stages.py
+++ analysis/utils/stages.py
@@ -1,5 +1,9 @@
 from enum import Enum, auto
 
+from analysis.utils.dirs import find_file
+
+stages_directory = find_file("config", "stages")
+
 
 class Stages(Enum):
     """
@@ -14,3 +18,41 @@ class Stages(Enum):
     stage2 = auto()
     final = auto()
     plot = auto()
+
+
+def check_for_unregistered_stage_file() -> bool:
+    """
+    This function serves to check if there exits any steering scripts inside the `config/stages` directory that are not
+    registered to the Stages enum.
+
+    Returns True if there is an unregistered steering script, otherwise return False
+    """
+    steering_script_names = [x.stem for x in stages_directory.glob("*.py")]
+    # Get all the valid prefixes
+    valid_prefixes = [variant.name for variant in Stages]
+    # Get a list of files that match the prefixes
+    valid_steering_scripts = [
+        name
+        for prefix in valid_prefixes
+        for name in steering_script_names
+        if name.startswith(prefix)
+    ]
+    # Finally, if the two lists aren't the same length then there is an additional unregistered script
+    return len(valid_steering_scripts) != len(steering_script_names)
+
+
+def get_stage_script(stage: Stages):
+    """
+    Gets the steering file for a given stage
+    """
+    assert isinstance(
+        stage, Stages
+    ), "The function get_stage_steering_script takes argument stage which must be of the enum type Stages"
+
+    stage_steering_file = [s for s in stages_directory.glob(f"{stage.name}*.py")]
+    if not stage_steering_file:
+        # No file was found
+        raise FileNotFoundError(
+            f"The steering file for stage '{stage.name}' was not found. Please ensure you have prefixed this stages steering file with '{stage.name}' and rerun."
+        )
+    return stage_steering_file[0]
