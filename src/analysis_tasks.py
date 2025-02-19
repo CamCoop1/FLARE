74d51b8c735a58f189fe0510b05b1a1948a5db3b
Cameron Cooper Harris <cameron.harris@adelaide.edu.au>
2025-02-19 15:33:44 +1030

    Begun the seperation of FCC analysis stages and mc production with the intention for them to be seperate workflows that can be connected if required

--- src/analysis_tasks.py
+++ src/analysis_tasks.py
@@ -5,21 +5,10 @@ from src.utils.tasks import FCCAnalysisRunnerBaseClass, OutputMixin
 from src.utils.yaml import get_config
 from src.utils.dirs import find_file
 
+
 details = get_config("details")
 
 results_subdir = f"{details['Name']}/{details['Version']}"
-
-
-class MCProduction(OutputMixin, FCCAnalysisRunnerBaseClass):
-    """
-    OPTIONAL This task will handle production of MC for FCC analyses
-    """
-
-    # TODO might have to overload the run_fcc_analysis function specifically for MC production as the cmd is unique
-
-    stage = Stages.mcproduction
-    results_subdir = results_subdir
-    cmd = ["DelphesPythia8_EDM4HEP"]
 
 
 class AnalysisStage1(OutputMixin, FCCAnalysisRunnerBaseClass):
