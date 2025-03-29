419790c17e5ef922747ba160b90b03cdda08e146
Cameron Cooper Harris <cameron.harris@adelaide.edu.au>
2025-03-29 18:29:19 +1030

    Centralised flare_config and dataprod_config to the __init__.py of src

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,11 @@
+from src.utils.dirs import find_file
 from src.utils.yaml import get_config
 
-details = get_config("details")
-results_subdir = f"{details['Name']}/{details['Version']}"
+flare_config = get_config("details")
+results_subdir = f"{flare_config['Name']}/{flare_config['Version']}"
+
+dataprod_dir = find_file(flare_config["StudyDir"], "mc_production")
+
+dataprod_config = (
+    get_config("details.yaml", dataprod_dir) if dataprod_dir.exists() else None
+)
