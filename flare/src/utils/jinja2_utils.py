6224b75306f1fb395c0438968ac5cc0245dfb0e3
Cameron Cooper Harris <cameron.harris@adelaide.edu.au>
2025-04-02 20:09:38 +1030

    Updated the get_template function to have an accurate default argument for dir

--- flare/src/utils/jinja2_utils.py
+++ flare/src/utils/jinja2_utils.py
@@ -3,7 +3,7 @@ import jinja2
 from flare.src.utils.dirs import find_file
 
 
-def get_template(name: str, dir="src/templates"):
+def get_template(name: str, dir="flare/src/templates"):
     loader = jinja2.FileSystemLoader(find_file(dir))
     env = jinja2.Environment(loader=loader)
     return env.get_template(name)
