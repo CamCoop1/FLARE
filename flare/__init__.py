46731b7edb8d0b3d7e7c9b8d74738d64175f5cb2
Cameron Cooper Harris <cameron.harris@adelaide.edu.au>
2025-04-03 14:11:08 +1030

    Created necessary functionality around the flare.process call to handle creating settings and different workflows

--- flare/__init__.py
+++ flare/__init__.py
@@ -1,3 +1,4 @@
+from b2luigi import get_setting
 from b2luigi import process as _process
 
 from flare.cli.utils import (
@@ -9,7 +10,20 @@ __all__ = ["process"]
 
 
 def process(*args, flare_args, **kwargs):
+    """
+    Here we wrap the b2luigi.process function to handle pre-run things that
+    need to be done before the actual b2luigi.process function is called
+    """
     load_settings_into_manager(flare_args)
     build_executable_and_save_to_settings_manager(flare_args)
+
+    if not kwargs.get("ignore_additional_command_line_args", None):
+        # Always set ignore_additional_command_line_args to True
+        kwargs["ignore_additional_command_line_args"] = True
+
+    if not kwargs.get("batch", None):
+        # In case the batch_system is set but the batch flag was not set True
+        if get_setting("batch_system", default=False):
+            kwargs["batch"] = True
 
     _process(*args, **kwargs)
--- flare/cli/arguments.py
+++ flare/cli/arguments.py
@@ -1,42 +1,89 @@
 import argparse
 from pathlib import Path
 
-from flare.cli.run_command import run_command
+from flare.cli.run_commands import run_analysis, run_mcproduction
+
+__all__ = ["ParserNames", "get_args"]
+
+
+class ParserNames:
+    pure_flare = "pure_flar"
+    flare = "flare"
+
+
+COMMON_ARGUMENTS = [
+    ("--name", {"help": "Name of the study"}),
+    ("--version", {"help": "Version of the study"}),
+    ("--description", {"help": "Description of the study"}),
+    (
+        "--study-dir",
+        {"help": "Study directory path where the files for production are located"},
+    ),
+    (
+        "--output-dir",
+        {
+            "help": "The location where the output file will be produced, by default will be the current working directory",
+            "default": Path().cwd(),
+        },
+    ),
+    ("--config-yaml", {"help": "Path to a YAML config file"}),
+]
+
+
+def add_common_arguments(parser):
+    for arg, options in COMMON_ARGUMENTS:
+        parser.add_argument(arg, **options)
 
 
 def get_args():
-    """Here we define the arguments for the arg parser and return the
-    parsed args"""
+    """Here we define the arg parser for the pure case in which a user wished to create their own custom flare workflow and
+    need to parse the commandline arguments to the prompt. For example, if a user defined a workflow like so:
 
-    def add_common_arguments(parser):
-        for arg, options in common_args:
-            parser.add_argument(arg, **options)
+    ``` python3
+    flare.process(
+        task_like_elements = MyB2luigiTask(),
+        workers=4,
+        batch=True,
+        ignore
 
-    parser = argparse.ArgumentParser(prog="flare", description="CLI for FLARE Project")
+    )
+    ```
+
+    ``` console
+    $ python3 run_my_custom_workflow.py --config-yaml analysis/config
+    ```
+
+    """
+    parser = argparse.ArgumentParser(
+        prog=ParserNames.pure_flare,
+        description="Pure CLI for running custom b2luigi workflow in FLARE",
+    )
+
+    add_common_arguments(parser)
+    parser.add_argument(
+        "--mcprod",
+        action="store_true",
+        help="If set, also run mcproduction as part of the analysis",
+    )
+
+    args = parser.parse_known_args()[0]
+    setattr(args, "prog", parser.prog)
+    return args
+
+
+def _get_args_cli():
+    """This arg parser function is used in conjunction with the flare CLI tool and is more robust than the
+    'pure' flare cli used inside get_args()."""
+
+    parser = argparse.ArgumentParser(
+        prog=ParserNames.flare, description="CLI for FLARE Project"
+    )
 
     subparsers = parser.add_subparsers(dest="command")
 
     # "run" command
     run_parser = subparsers.add_parser("run", help="Run the flare command")
     run_subparsers = run_parser.add_subparsers(dest="subcommand")
-    # Common arguments
-    common_args = [
-        ("--name", {"help": "Name of the study"}),
-        ("--version", {"help": "Version of the study"}),
-        ("--description", {"help": "Description of the study"}),
-        (
-            "--study-dir",
-            {"help": "Study directory path where the files for production are located"},
-        ),
-        (
-            "--output-dir",
-            {
-                "help": "The location where the output file will be produced, by default will be the current working directory",
-                "default": Path().cwd(),
-            },
-        ),
-        ("--config-yaml", {"help": "Path to a YAML config file"}),
-    ]
 
     # "analysis" subcommand
     analysis_parser = run_subparsers.add_parser(
@@ -48,7 +95,7 @@ def get_args():
         action="store_true",
         help="If set, also run mcproduction as part of the analysis",
     )
-    analysis_parser.set_defaults(func=run_command)
+    analysis_parser.set_defaults(func=run_analysis)
 
     # "mcproduction" subcommand
     mcprod_parser = run_subparsers.add_parser(
@@ -62,6 +109,8 @@ def get_args():
         help=argparse.SUPPRESS,  # Hide from the help menu
     )
 
-    mcprod_parser.set_defaults(func=run_command)
+    mcprod_parser.set_defaults(func=run_mcproduction)
 
-    return parser.parse_known_args()[0]
+    args = parser.parse_known_args()[0]
+    setattr(args, "prog", parser.prog)
+    return args
