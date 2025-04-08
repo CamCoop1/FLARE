a1eff17793445a555f78f9d6a74fed3ee34f0b7d
Cameron Cooper Harris <cameron.harris@adelaide.edu.au>
2025-04-08 20:14:03 +0930

    Added pydantic validation to UserMCProdConfigModel to ensure if a global_prodtype is set, that the datatype is in the correct format

--- flare/src/pydantic_models/user_mcprod_config_model.py
+++ flare/src/pydantic_models/user_mcprod_config_model.py
@@ -25,31 +25,39 @@ class UserMCProdConfigModel(ForbidExtraBaseModel):
         prodtype = values.get("global_prodtype")
         datatype = values.get("datatype")
 
-        if prodtype == "default":
-            if not isinstance(datatype, list):
-                raise ValueError("datatype must be a list")
-            for item in datatype:
-                if not isinstance(item, dict):
+        if prodtype != "default":
+            assert isinstance(
+                datatype, list
+            ), "When setting a global_prodtype, the datatype must be a list"
+            assert all(
+                isinstance(x, str) for x in datatype
+            ), "When setting a global_prodtype, each type in the datatype list must be a string"
+            return values
+
+        if not isinstance(datatype, list):
+            raise ValueError("datatype must be a list")
+        for item in datatype:
+            if not isinstance(item, dict):
+                raise ValueError(
+                    "When prodtype is 'default', each datatype must be a dictionary e.g {'my_data' : {'prodtype': 'whizard'}}"
+                )
+            if len(item) != 1:
+                raise ValueError(
+                    "Each datatype dictionary must have exactly one key e.g {'my_data' : {'prodtype': 'whizard'}}"
+                )
+            for val in item.values():
+                if not isinstance(val, dict):
                     raise ValueError(
-                        "When prodtype is 'default', each datatype must be a dictionary e.g {'my_data' : {'prodtype': 'whizard'}}"
+                        "The value of each datatype entry must be a dictionary e.g {'my_data' : {'prodtype': 'whizard'}}"
                     )
-                if len(item) != 1:
+                inner_prodtype = val.get("prodtype", None)
+                if not inner_prodtype:
                     raise ValueError(
-                        "Each datatype dictionary must have exactly one key e.g {'my_data' : {'prodtype': 'whizard'}}"
+                        "There is no prodtype in the datatype dictionary e.g {'my_data' : {'prodtype': 'whizard'}} "
                     )
-                for val in item.values():
-                    if not isinstance(val, dict):
-                        raise ValueError(
-                            "The value of each datatype entry must be a dictionary e.g {'my_data' : {'prodtype': 'whizard'}}"
-                        )
-                    inner_prodtype = val.get("prodtype", None)
-                    if not inner_prodtype:
-                        raise ValueError(
-                            "There is no prodtype in the datatype dictionary e.g {'my_data' : {'prodtype': 'whizard'}} "
-                        )
-                    prodtypes = ("madgraph", "whizard", "pythia8")
-                    if inner_prodtype not in prodtypes:
-                        raise ValueError(
-                            f"Invalid prodtype '{inner_prodtype}' in datatype entry. Valid types are {', '.join(prodtypes)}"
-                        )
+                prodtypes = ("madgraph", "whizard", "pythia8")
+                if inner_prodtype not in prodtypes:
+                    raise ValueError(
+                        f"Invalid prodtype '{inner_prodtype}' in datatype entry. Valid types are {', '.join(prodtypes)}"
+                    )
         return values
