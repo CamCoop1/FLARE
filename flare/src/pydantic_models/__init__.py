01d0c62bbe7443b8f6679c2e6f2728d22980ea69
Cameron Cooper Harris <cameron.harris@adelaide.edu.au>
2026-01-15 21:06:27 +1030

    Added UserConfigModel

--- flare/src/pydantic_models/__init__.py
+++ flare/src/pydantic_models/__init__.py
@@ -2,9 +2,15 @@ from flare.src.pydantic_models.production_types_model import (
     FCCProductionModel,
     MCProductionModel,
 )
+from flare.src.pydantic_models.user_config_model import UserConfigModel
 from flare.src.pydantic_models.user_mcprod_config_model import UserMCProdConfigModel
 
 models = dict()
 
-for model in [FCCProductionModel, MCProductionModel, UserMCProdConfigModel]:
+for model in [
+    FCCProductionModel,
+    MCProductionModel,
+    UserMCProdConfigModel,
+    UserConfigModel,
+]:
     models[model.__name__] = model
---
+++ flare/src/pydantic_models/user_config_model.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+from typing import Dict, List, Optional
+
+from pydantic import BaseModel, Field, root_validator
+
+from flare.src.pydantic_models.utils import StageModel
+
+
+class AddStageModel(StageModel):
+    required_by: List[str] = Field(default_factory=list)
+    requires: List[str] = Field(default_factory=list)
+
+    @root_validator
+    def check_at_least_one(cls, values):
+        if not values.get("required_by") and not values.get("requires"):
+            raise ValueError(
+                "At least one of 'required_by' or 'requires' must be provided"
+            )
+        return values
+
+
+class UserConfigModel(BaseModel):
+    name: str = Field(default="default_name")
+    version: str = Field(default="1.0")
+    description: str = Field(default="No Description")
+    studydir: Path | str = Field(default_factory=Path.cwd)
+    outputdir: Path | str = Field(default_factory=Path.cwd)
+    add_stage: Optional[Dict[str, AddStageModel]] = Field(default_factory=dict)
+
+    class Config:
+        extra = "allow"
+
+    @property
+    def extra_config_settings(self):
+        """Here we capture any additional arguments set by the user
+        Specifically this would be things like:
+        ```YAML
+        batch_system = 'slurm',
+
+        ```
+        """
+        return {k: v for k, v in self.__dict__.items() if k not in self.__fields__}
