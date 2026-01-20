4710b44fbdd310d74ba7a13c5718b7b14427a5ae
Cameron Cooper Harris <cameron.harris@adelaide.edu.au>
2026-01-20 16:32:25 +1030

    Updated UserConfigModel and AddStageModel with additional field_validators

--- flare/src/pydantic_models/user_config_model.py
+++ flare/src/pydantic_models/user_config_model.py
@@ -1,7 +1,8 @@
+from collections import Counter
 from pathlib import Path
 from typing import Dict, List, Optional
 
-from pydantic import ConfigDict, BaseModel, Field, model_validator, field_validator
+from pydantic import BaseModel, ConfigDict, Field, field_validator, model_validator
 
 from flare.src.pydantic_models.utils import StageModel
 
@@ -10,13 +11,23 @@ class AddStageModel(StageModel):
     required_by: List[str] = Field(default_factory=list)
     requires: str = Field(default_factory=str)
 
-    @model_validator(mode="after")
+    @model_validator(mode="before")
     def check_at_least_one(cls, values):
         if not values.get("required_by") and not values.get("requires"):
             raise ValueError(
                 "At least one of 'required_by' or 'requires' must be provided"
             )
         return values
+
+    @field_validator("requires", mode="after")
+    @classmethod
+    def set_requires_string_to_lower(cls, value):
+        return value.lower()
+
+    @field_validator("required_by", mode="after")
+    @classmethod
+    def set_required_by_string_to_lower(cls, model):
+        return [x.lower() for x in model]
 
 
 class UserConfigModel(BaseModel):
@@ -30,11 +41,25 @@ class UserConfigModel(BaseModel):
 
     # TODO[pydantic]: We couldn't refactor the `validator`, please replace it by `field_validator` manually.
     # Check https://docs.pydantic.dev/dev-v2/migration/#changes-to-validators for more information.
-    @field_validator("add_stage", mode='after')
+    @field_validator("add_stage", mode="before")
     @classmethod
     def lowercase_keys(cls, v):
         if isinstance(v, dict):
             return {k.lower(): v for k, v in v.items()}
+        return v
+
+    @field_validator("add_stage", mode="after")
+    @classmethod
+    def check_no_two_stages_have_same_required_by(cls, v):
+        all_required_stages = [
+            task for value in v.values() for task in value.required_by
+        ]
+        counted_required_stages = Counter(all_required_stages)
+        multiple_counts = [x for x, y in counted_required_stages.items() if y > 1]
+        assert len(multiple_counts) == 0, (
+            f"You have defined more than one add_stage value with overlapping required_by values {multiple_counts}. "
+            "FLARE does not allow a Task to have more than one requirement. Fix this and try again"
+        )
         return v
 
     @property
